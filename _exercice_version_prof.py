cc9e4e99380ec85da944d94f1adccf04d4307a88
nayarramo <nayarramo@outlook.com>
2022-10-17 13:50:05 -0400

    fin

--- _exercice_version_prof.py
+++ _exercice_version_prof.py
@@ -35,9 +35,9 @@ def color_name_to_hex(colors: list) -> list:
 
 
 def create_list() -> list:
-    # TODO: Créer une liste des 10 000 premiers entiers positif, sauf pour les entiers de 15 à 350
+    # TODO: Créer une liste des 1 000 premiers entiers positif, sauf pour les entiers de 15 à 350
 
-    return [number for number in range(10000) if number < 15 or number > 350]
+    return [number for number in range(1000) if number < 15 or number > 350]
 
 
 def compute_mse(model_dict: dict) -> dict:
@@ -59,7 +59,7 @@ def main() -> None:
     colors = ["blue", "red", "green", "yellow", "black", "white"]
     print(f"La valeur hex associée aux couleurs est: {color_name_to_hex(colors)}")
 
-    print(f"La liste des 10000 entiers est: {create_list()}")
+    print(f"La liste des 1000 entiers est: {create_list()}")
 
     model_dict = {"LR": [(90, 92), (96, 100), (20, 25), (21, -2), (3, -20)],
                   "DNN": [(100, 101), (50, 50), (1,2), (-10, -12), (-1, 7)],
--- exercice.py
+++ exercice.py
@@ -1,31 +1,53 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from pyexpat import model
 from matplotlib.colors import cnames
 
 
 def list_to_dict(some_list: list) -> dict:
     # TODO: Transformer la liste en dictionnaire, les éléments de la liste deviennent les clés et leur index deviennent les valeurs
+    # dic = {}
+    # for i in range(len(some_list)):
+    #     dic[some_list[i]] = i
+    # return dic
 
-    return {}
+    return {key: some_list.index(key) for key in some_list}
 
 
 def color_name_to_hex(colors: list) -> list:
     # TODO: Trouver la valeur hex de chaque couleur dans la liste et créer une liste de tupple où le premier élément est le nom de la couleur et le deuxième est la valeur hex
-
-    return []
+    # result = []
+    # for color in colors:
+    #     result.append((color, cnames[color]))
+    # return result
+    return [(color, cnames[color]) for color in colors]
 
 
 def create_list() -> list:
-    # TODO: Créer une liste des 10 000 premiers entiers positif, sauf pour les entiers de 15 à 350
+    # TODO: Créer une liste des 1 000 premiers entiers positif, sauf pour les entiers de 15 à 350
+    # times, number, result = 0, 0, []
+    # while times < 1000:
+    #     if number < 15 or number > 350:
+    #         result.append(number)
+    #         times += 1
+    #     number += 1
+    # return result
 
-    return []
+    return [nb for nb in range(1336) if not 15<=nb<=350]
 
 
 def compute_mse(model_dict: dict) -> dict:
     # TODO: Calculer l'erreur quadratique moyen pour chaque modèle. Retourner un dictionnaire contenant les MSE.
+    result = {}
 
-    return {}
+    for key, value in model_dict.items():
+        total = 0
+        for tuple in value:
+            total += (tuple[1]-tuple[0])**2
+        result[key] = total/(len(value))
+
+    return result
 
 
 def main() -> None:
@@ -35,7 +57,7 @@ def main() -> None:
     colors = ["blue", "red", "green", "yellow", "black", "white"]
     print(f"La valeur hex associée aux couleurs est: {color_name_to_hex(colors)}")
 
-    print(f"La liste des 10000 entiers est: {create_list()}")
+    print(f"La liste des 1000 entiers est: {create_list()}")
 
     model_dict = {"LR": [(90, 92), (96, 100), (20, 25), (21, -2), (3, -20)],
                   "DNN": [(100, 101), (50, 50), (1,2), (-10, -12), (-1, 7)],
